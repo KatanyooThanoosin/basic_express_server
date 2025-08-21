5248c5b823eb4377e4e17f7e08b901f9741d3e28
Katanyoo Thanoosin <nongjupz@MacBook-Air-khxng-Katanyoo.local>
2025-08-21 09:45:39 +0700

    add try catch to controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,32 @@ import userService from "../services/userService.js"
 
 const userController = {
   getAllUsers: async (req, res) => {
-    const users = await userService.getAllUsers()
-    res.status(200).json(users)
+    try{
+      const users = await userService.getAllUsers()
+      res.status(200).json(users)
+    } catch(err) {
+      res.status(500).json(err)
+    }
   },
   getUserById: async (req, res) => {
-    const id = req.params.id
-    const user = await userService.getUserById(id)
-    res.status(200).json(user)
+    try {
+      const id = req.params.id
+      const user = await userService.getUserById(id)
+      res.status(200).json(user)
+    } catch(err) {
+      res.status(500).json(err)
+    }
+    
   },
   create: async (req, res) => {
-    const { name, age, phone } = req.body
-    const user = await userService.create(name, age, phone)
-    res.status(201).json(user)
+    try{
+      const { name, age, phone } = req.body
+      const user = await userService.create(name, age, phone)
+      res.status(201).json(user)
+    } catch(err){
+      res.status(500).json(err)
+    }
+    
   }
 }
 
