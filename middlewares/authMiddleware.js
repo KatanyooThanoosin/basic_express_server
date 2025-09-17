bb6e05d78e6ce7b62d003f1a6384b63b91b54f5c
kty_japz <88848316+KatanyooThanoosin@users.noreply.github.com>
2025-09-17 17:37:29 +0700

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken"
 import userService from "../services/userService.js";
 
 const authMiddleware = () => {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     try {
       const jwt_secret = process.env.JWT_SECRET;
       const tokenArray = req.headers['authorization'].split(' ');
       const decodedToken = jwt.verify(tokenArray[1], jwt_secret);
-      const user = userService.getUserById(decodedToken.userId);
+      const user = await userService.getUserById(decodedToken.userId);
       if(user){
         req['user'] = decodedToken.userId;
         next();
